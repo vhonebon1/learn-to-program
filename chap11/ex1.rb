24188f284c1bce9ca689a4da409c6441d2c5d936
Verity <vhonebon@gmail.com>
2017-05-21 22:23:37 +0100

    changes to exercise 2 of chapter 11

--- chap11/ex1.rb
+++ chap11/ex1.rb
@@ -1,22 +1,25 @@
-Dir.chdir 'user/Desktop/pictures'
-pic_names = Dir['F:/**/*.jpg']
-puts "What would you call this batch?"
+Dir.chdir '/Users/user/Desktop/Pictures'
+pic_names = Dir['/Users/user/Desktop/Pictures/*.jpg']
+puts "What would you like to call this batch?"
 batch_name = gets.chomp
+
 if File.exist?(batch_name) == true
     puts "This name already exists, please enter a valid name"
+    exit
 end
-print "Downloading #{pic_name.length} files: "
+
+puts
+print "Downloading #{pic_names.length} files: "
 pic_number = 1
 pic_names.each do |name|
     print '.'
-    new_name = if pic_numver < 10
-    "#{batch_name}0#{pic_number}.jpg"
-else
-    "#{batch_name}#{pic_number}.jpg"
-else
-    "#{batch_name}#{pic_number}.jpg"
+	new_name = if pic_number < 10
+    	"#{batch_name}0#{pic_number}.jpg"
+	else
+    	"#{batch_name}#{pic_number}.jpg"
+	end
+	File.rename name, new_name
+	pic_number = pic_number + 1
 end
-File.rename name, new_name
-pic_number = pic_number + 1
-end
+puts
 puts "Task completed"
